457256095b55465538585084d453c3cb4401e543
kaif siddiqui <kaifs8998@gmail.com>
2024-02-26 23:50:23 +0530

    some minor changes

--- app/components/carousel.jsx
+++ app/components/carousel.jsx
@@ -4,72 +4,78 @@ const carousel = () => {
   return (
     <>
       <div
-        id="carouselExampleIndicators"
-        class="carousel slide"
+        id="carouselExampleAutoplaying"
+        className="carousel slide"
         data-bs-ride="carousel"
       >
-        <div class="carousel-indicators">
+        <div className="carousel-indicators">
           <button
             type="button"
-            data-bs-target="#carouselExampleIndicators"
+            data-bs-target="#carouselExampleAutoplaying"
             data-bs-slide-to="0"
-            class="active"
+            className="active"
             aria-current="true"
             aria-label="Slide 1"
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleIndicators"
+            data-bs-target="#carouselExampleAutoplaying"
             data-bs-slide-to="1"
             aria-label="Slide 2"
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleIndicators"
+            data-bs-target="#carouselExampleAutoplaying"
             data-bs-slide-to="2"
             aria-label="Slide 3"
           ></button>
         </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="4000">
             <img
               src="https://wowslider.com/sliders/demo-93/data1/images/lake.jpg"
-              class="d-block w-100"
+              className="d-flex w-100 "
               alt="..."
             />
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item" data-bs-interval="4000">
             <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhc59rnKe-EAJUN-kjca_CWtZoE4YZLth3utzaB2Sy3Q&s"
-              class="d-block w-100"
+              src="https://wowslider.com/sliders/demo-93/data1/images/lake.jpg"
+              className="d-flex w-100 "
               alt="..."
             />
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item" data-bs-interval="4000">
             <img
-              src="https://fastly.picsum.photos/id/54/1024/480.jpg?hmac=MfVNcs6pENu_41pux1kb9sLGUEvwhioKVpsoGgZUm_c"
-              class="d-block w-100"
+              src="https://wowslider.com/sliders/demo-93/data1/images/lake.jpg"
+              className="d-flex w-100 "
               alt="..."
             />
           </div>
         </div>
         <button
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           type="button"
-          data-bs-target="#carouselExampleIndicators"
+          data-bs-target="#carouselExampleAutoplaying"
           data-bs-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
         </button>
         <button
-          class="carousel-control-next"
+          className="carousel-control-next"
           type="button"
-          data-bs-target="#carouselExampleIndicators"
+          data-bs-target="#carouselExampleAutoplaying"
           data-bs-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
     </>
