42d7296f0d83739e91d17d2a21dbdc19bcb4ee8a
kaif siddiqui <kaifs8998@gmail.com>
2024-05-28 23:33:30 +0530

    feat-created navbar

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { PiBowlFoodFill } from "react-icons/pi";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaSearch, FaUserCircle } from "react-icons/fa";
 
 const nav = () => {
-  return <>Hello, this is Nav component.</>;
+  return (
+    <>
+      <header>
+        <div className="nav-wrapper">
+          <div className="nav flex justify-between items-center p-5">
+            <div className="nav-icon flex justify-center items-center">
+              <PiBowlFoodFill size={40} />
+              <span>FoodBowl</span>
+            </div>
+            <div className="nav-items flex gap-6 justify-between items-center">
+              <span className=" flex gap-2">
+                Search
+                <FaSearch size={25} />
+              </span>
+              <ul className=" flex items-center gap-4">
+                <li>
+                  <a href="/">Login</a>
+                </li>
+                <li>
+                  <a href="/">Signup</a>
+                </li>
+                <li>
+                  <a href="/"></a>
+                  <FaCartShopping size={30} />
+                </li>
+              </ul>
+              <span className="flex">
+                <FaUserCircle size={30} />
+              </span>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default nav;
