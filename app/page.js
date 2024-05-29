e06c6736b36fb1ff992f0315606dd735b8bbad62
kaif siddiqui <kaifs8998@gmail.com>
2024-05-29 23:59:37 +0530

    refactor-updated foodMenu component import

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Nav from "./components/nav";
-import Carousel from "./components/carousel";
+import FoodMenu from "./components/foodMenu";
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <Carousel />
+      <FoodMenu />
     </>
   );
 }
