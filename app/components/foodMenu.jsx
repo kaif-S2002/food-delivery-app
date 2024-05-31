39174cc80ef29c571723b9965032bec3195a39c2
kaif siddiqui <kaifs8998@gmail.com>
2024-05-31 23:53:14 +0530

    fix-made 'food-name' visible & updated gap value of food-container

--- app/components/foodMenu.jsx
+++ app/components/foodMenu.jsx
@@ -2,58 +2,66 @@ import Image from "next/image";
 
 const FoodMenu = () => {
   return (
-    <div className="foodMenu-wrapper flex flex-col justify-center items-center mx-auto p-4 pt-6 md:p-6 lg:p-12">
+    <div className="foodMenu-wrapper flex flex-col justify-center items-center mx-auto p-4 pt-8">
       <h1 className="title text-3xl font-bold mb-8">What's on your mind?</h1>
-      <div className="foods-container flex justify-center items-center h-fit w-fit gap-5 max-lg:grid max-md:grid-cols-2 max-lg:grid-cols-4 ">
-        <div className="food shadow-lg w-40 h-40 rounded-full object-cover overflow-hidden">
+      <div className="foods-container flex justify-center items-center h-fit w-fit gap-x-8 gap-y-5 max-lg:grid max-md:grid-cols-2 max-lg:grid-cols-4 ">
+        <div className="food ">
           <Image
             src="/assets/vegThali.png"
             alt="Veg Thali"
             width={200}
             height={200}
-            className="food-image w-full h-full "
+            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
           />
-          <p className="food-name text-lg font-bold mt-2">Veg</p>
+          <p className="food-name text-base font-bold mt-2 text-center">Veg</p>
         </div>
-        <div className="food shadow-lg w-40 h-40 rounded-full object-cover overflow-hidden">
+        <div className="food ">
           <Image
             src="/assets/NonVeg.png"
             alt="Non-veg Thali"
             width={200}
             height={200}
-            className="food-image w-full h-full object-cover rounded"
+            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
           />
-          <p className="food-name text-lg font-bold mt-2">Non-Veg</p>
+          <p className="food-name text-base font-bold mt-2 text-center">
+            Non-Veg
+          </p>
         </div>
-        <div className="food shadow-lg w-40 h-40 rounded-full object-cover overflow-hidden">
+        <div className="food ">
           <Image
             src="/assets/parantha.png"
             alt="Breakfast"
             width={200}
             height={200}
-            className="food-image w-full h-full object-cover rounded"
+            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
           />
-          <p className="food-name text-lg font-bold mt-2">Breakfast</p>
+          <p className="food-name text-base font-bold mt-2 text-center">
+            Breakfast
+          </p>
         </div>
-        <div className="food shadow-lg w-40 h-40 rounded-full object-cover overflow-hidden">
+        <div className="food ">
           <Image
             src="/assets/briyani.png"
             alt="Briyani"
             width={200}
             height={200}
-            className="food-image w-full h-full object-cover rounded"
+            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
           />
-          <p className="food-name text-lg font-bold mt-2">Briyani</p>
+          <p className="food-name text-base font-bold mt-2 text-center">
+            Briyani
+          </p>
         </div>
-        <div className="food shadow-lg w-40 h-40 rounded-full object-cover overflow-hidden">
+        <div className="food ">
           <Image
             src="/assets/Softdrinks.png"
             alt="Soft drinks"
             width={200}
             height={200}
-            className="food-image w-full h-full object-cover rounded"
+            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
           />
-          <p className="food-name text-lg font-bold mt-2">Soft Drinks</p>
+          <p className="food-name text-base font-bold mt-2 text-center">
+            Soft Drinks
+          </p>
         </div>
       </div>
     </div>
