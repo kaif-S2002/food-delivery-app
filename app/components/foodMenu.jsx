4276ba12b546afc93badedb0f7027425dfd6d92e
kaif siddiqui <kaifs8998@gmail.com>
2024-06-06 00:15:04 +0530

    feat-created 'food' to an anchor tag & refactor-changed 'coldDrink' image and added user-select-none property

--- app/components/foodMenu.jsx
+++ app/components/foodMenu.jsx
@@ -1,67 +1,80 @@
+import React from "react";
 import Image from "next/image";
 
 const FoodMenu = () => {
   return (
-    <div className="foodMenu-wrapper flex flex-col justify-center items-center mx-auto p-4 pt-8">
-      <h1 className="title text-3xl font-bold mb-8">What's on your mind?</h1>
-      <div className="foods-container flex justify-center items-center h-fit w-fit gap-x-8 gap-y-5 max-lg:grid max-md:grid-cols-2 max-lg:grid-cols-4 ">
+    <div className="foodMenu-wrapper flex flex-col justify-center items-center mx-auto p-4 pt-12">
+      <h1 className="title text-3xl font-bold mb-8">Categories</h1>
+      <div className="foods-container flex justify-center items-center h-fit w-fit gap-x-8 gap-y-5 select-none max-lg:grid max-md:grid-cols-2 max-lg:grid-cols-4">
         <div className="food ">
-          <Image
-            src="/assets/vegThali.png"
-            alt="Veg Thali"
-            width={200}
-            height={200}
-            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
-          />
-          <p className="food-name text-base font-bold mt-2 text-center">Veg</p>
+          <a href="/veg-menu">
+            <Image
+              src="/assets/vegThali.png"
+              alt="Veg Thali"
+              width={200}
+              height={200}
+              className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
+            />
+            <p className="food-name text-base font-bold mt-2 text-center">
+              Veg
+            </p>
+          </a>
         </div>
         <div className="food ">
-          <Image
-            src="/assets/NonVeg.png"
-            alt="Non-veg Thali"
-            width={200}
-            height={200}
-            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
-          />
-          <p className="food-name text-base font-bold mt-2 text-center">
-            Non-Veg
-          </p>
+          <a href="/non-veg-menu">
+            <Image
+              src="/assets/NonVeg.png"
+              alt="Non-veg"
+              width={200}
+              height={200}
+              className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
+            />
+            <p className="food-name text-base font-bold mt-2 text-center">
+              Non-Veg
+            </p>
+          </a>
         </div>
         <div className="food ">
-          <Image
-            src="/assets/parantha.png"
-            alt="Breakfast"
-            width={200}
-            height={200}
-            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
-          />
-          <p className="food-name text-base font-bold mt-2 text-center">
-            Breakfast
-          </p>
+          <a href="/briyani-menu">
+            <Image
+              src="/assets/briyani.png"
+              alt="Briyani"
+              width={200}
+              height={200}
+              className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
+            />
+            <p className="food-name text-base font-bold mt-2 text-center">
+              Briyani
+            </p>
+          </a>
+        </div>
+        <div className="food w-fit h-fit">
+          <a href="/parantha-menu">
+            <Image
+              src="/assets/parantha.png"
+              alt="Parantha"
+              width={200}
+              height={200}
+              className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
+            />
+            <p className="food-name text-base font-bold mt-2 text-center">
+              Parantha
+            </p>
+          </a>
         </div>
         <div className="food ">
-          <Image
-            src="/assets/briyani.png"
-            alt="Briyani"
-            width={200}
-            height={200}
-            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
-          />
-          <p className="food-name text-base font-bold mt-2 text-center">
-            Briyani
-          </p>
-        </div>
-        <div className="food ">
-          <Image
-            src="/assets/Softdrinks.png"
-            alt="Soft drinks"
-            width={200}
-            height={200}
-            className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
-          />
-          <p className="food-name text-base font-bold mt-2 text-center">
-            Soft Drinks
-          </p>
+          <a href="/softdrinks-menu">
+            <Image
+              src="/assets/coldDrink.png"
+              alt="Soft Drinks"
+              width={200}
+              height={200}
+              className="food-image shadow-lg w-36 h-36 rounded-full object-cover"
+            />
+            <p className="food-name text-base font-bold mt-2 text-center">
+              Soft Drinks
+            </p>
+          </a>
         </div>
       </div>
     </div>
