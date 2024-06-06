7157c43514c85c15a11cc715fbbaed1083567b7e
kaif siddiqui <kaifs8998@gmail.com>
2024-06-06 23:02:03 +0530

    refactor-created 'pages' folder and inside it 'home.jsx' file

---
+++ app/pages/home.jsx
@@ -0,0 +1,16 @@
+import Nav from "../components/nav";
+import FoodMenu from "../components/foodMenu";
+import Banner from "../components/banner";
+import MenuList from "../components/menuList";
+
+const Home = () => {
+  return (
+    <>
+      <Nav />
+      <Banner />
+      <FoodMenu />
+      <MenuList />
+    </>
+  );
+};
+export default Home;
