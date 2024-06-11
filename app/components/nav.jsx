8eb4cebf36578f098526a8aa296226c2b4cb61f8
kaif siddiqui <kaifs8998@gmail.com>
2024-06-11 23:59:32 +0530

    feat-added conditional rendering on admin link of nav

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -5,14 +5,17 @@ import { IoIosSearch } from "react-icons/io";
 import { CiShoppingCart } from "react-icons/ci";
 import { FaUserCircle } from "react-icons/fa";
 import { RiAdminFill } from "react-icons/ri";
+import DropDownMenu from "./dropDownMenu";
 
-const nav = () => {
-  const [token, setToken] = useState(false);
+const nav = (props) => {
+  const [token, setToken] = useState("adminToken", true);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <header>
         <div className="nav-wrapper w-full h-fit fixed z-10 shadow top-0">
-          <div className="nav flex justify-between items-center py-5 px-10 font-semibold overflow-hidden select-none">
+          <div className="nav flex justify-between items-center py-4 px-10 font-semibold overflow-hidden select-none max-sm:px-5">
             <div className="nav-icon flex justify-center items-center text-sm text-green-700 font-serif font-bold cursor-pointer">
               <a href="" className=" flex justify-center items-center">
                 <span>FOOD</span>
@@ -20,25 +23,29 @@ const nav = () => {
                 <span>BOWL</span>
               </a>
             </div>
-            <div className="nav-items flex gap-10 justify-between items-center max-lg:hidden">
-              <span className=" flex gap-1 cursor-pointer">
+            <div className="nav-items flex gap-14 justify-between items-center txet-sm tracking-wider max-sm:gap-8">
+              <span className=" flex gap-1 cursor-pointer max-md:hidden">
                 Search
                 <IoIosSearch size={24} />
               </span>
-              <span className="cart cursor-pointer">
+              <span className="cart cursor-pointer max-md:hidden">
                 <a href="/"></a>
                 <CiShoppingCart size={30} />
               </span>
-              <span className=" flex gap-1 cursor-pointer">
-                Admin <RiAdminFill size={20} />
-              </span>
-              {token ? (
+              {token === "adminToken" ? (
+                <span className=" flex gap-1 cursor-pointer">
+                  Admin
+                  <RiAdminFill size={18} className=" mt-0.5" />
+                </span>
+              ) : (
+                ""
+              )}
+              {!token ? (
                 <>
-                  <span>
-                    {" "}
-                    <a href="/">Login</a>{" "}
+                  <span className=" max-md:hidden">
+                    <a href="/">Login</a>
                   </span>
-                  <span>
+                  <span className=" max-md:hidden">
                     <a href="/">Signup</a>
                   </span>
                 </>
@@ -46,9 +53,15 @@ const nav = () => {
                 ""
               )}
 
-              <span className="flex cursor-pointer">
+              <span
+                className="flex cursor-pointer relative"
+                onClick={() => setIsOpen(!isOpen)}
+              >
                 <FaUserCircle size={30} />
               </span>
+              <DropDownMenu
+                visible={isOpen ? "top-20 right-0" : "top-20 right-0 hidden"}
+              />
             </div>
           </div>
         </div>
