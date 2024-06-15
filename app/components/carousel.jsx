01b91f0619e9bfedd6f41921d9f19c386d211ce4
kaif siddiqui <kaifs8998@gmail.com>
2024-06-15 23:59:32 +0530

    feat-made carousel responsive for all screens

--- app/components/carousel.jsx
+++ app/components/carousel.jsx
@@ -1,18 +1,87 @@
+"use client";
 import React from "react";
-import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const banner = () => {
+const Carousel = () => {
+  let settings = {
+    className: "center",
+    centerMode: true,
+    dots: true,
+    infinite: true,
+    centerPadding: "180px",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 10000,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1536,
+        settings: {
+          centerPadding: "260px",
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          centerPadding: "220px",
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          centerPadding: "160px",
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: "80px",
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerPadding: "25px",
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const carouselData = [
+    "Tasty food for every mood.",
+    " Food Is A Never Ending Story.",
+    "Beware Of Expensive Food.",
+  ];
   return (
-    <>
-      <div className="banner-wrapper overflow-hidden">
-        <div className="banner flex justify-center items-center h-fit w-full text-white ">
-          <h1 className="banner-heading select-none max-lg:text-8xl max-sm:text-7xl max-lg:pl-10">
-            FOOD BOWL.
-          </h1>
-        </div>
-      </div>
-    </>
+    <div className="slider-container mt-24 h-64 overflow-hidden">
+      <Slider {...settings}>
+        {carouselData.map((data) => {
+          return (
+            <div className=" h-full w-full p-4 rounded-xl text-white shadow-xl bg-neutral-500 overflow-hidden">
+              <p className=" flex justify-center items-center text-6xl font-bold h-full">
+                {data}
+              </p>
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
   );
 };
 
-export default banner;
+export default Carousel;
