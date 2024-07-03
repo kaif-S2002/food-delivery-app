c29d319ba200228525f0a451d825122ce3fce2c1
kaif siddiqui <kaifs8998@gmail.com>
2024-07-03 23:57:51 +0530

    refactor-created screens folder and stored 'main.jsx' file in components

---
+++ app/components/screens/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Carousel from "../carousel";
+import Categories from "../categories";
+import MenuList from "../menuList";
+
+const page = () => {
+  return (
+    <>
+      <Carousel />
+      <Categories />
+      <MenuList />
+    </>
+  );
+};
+
+export default page;
