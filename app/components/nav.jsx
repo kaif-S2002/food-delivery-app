40992923551e91d554c9d1d1d98761f479d33da8
kaif siddiqui <kaifs8998@gmail.com>
2024-07-15 00:45:52 +0530

    feat-added conditional rendering on 'login/logout' button and 'user-icon' using session provided by next-auth

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -5,16 +5,18 @@ import { IoIosSearch } from "react-icons/io";
 import { CiShoppingCart } from "react-icons/ci";
 import { FaUserCircle } from "react-icons/fa";
 import { RiAdminFill } from "react-icons/ri";
+import { useSession } from "next-auth/react";
 import DropDownMenu from "./dropDownMenu";
 
 const nav = (props) => {
-  const [token, setToken] = useState("adminToken", true);
+  const [token, setToken] = useState();
   const [isOpen, setIsOpen] = useState(false);
+  const { status } = useSession();
 
   return (
     <>
       <header>
-        <div className="nav-wrapper w-full h-fit fixed z-10 shadow top-0">
+        <div className="nav-wrapper w-full h-fit fixed z-10 shadow top-0 bg-gray-100">
           <div className="nav flex justify-between items-center py-4 px-10 font-semibold overflow-hidden select-none max-sm:px-5">
             <div className="nav-icon flex justify-center items-center text-sm text-green-700 font-serif font-bold cursor-pointer">
               <a href="" className=" flex justify-center items-center">
@@ -23,7 +25,7 @@ const nav = (props) => {
                 <span>BOWL</span>
               </a>
             </div>
-            <div className="nav-items flex gap-14 justify-between items-center txet-sm tracking-wider max-sm:gap-8">
+            <div className="nav-items flex gap-12 justify-between items-center txet-sm tracking-wider max-sm:gap-8">
               <span className=" flex gap-1 cursor-pointer max-md:hidden">
                 Search
                 <IoIosSearch size={24} />
@@ -40,13 +42,13 @@ const nav = (props) => {
               ) : (
                 ""
               )}
-              {!token ? (
+              {status !== "authenticated" ? (
                 <>
                   <span className=" max-md:hidden">
-                    <a href="/">Login</a>
+                    <a href="/login">Login</a>
                   </span>
                   <span className=" max-md:hidden">
-                    <a href="/">Signup</a>
+                    <a href="/register">Signup</a>
                   </span>
                 </>
               ) : (
@@ -54,8 +56,10 @@ const nav = (props) => {
               )}
 
               <span
-                className="flex cursor-pointer relative"
-                onClick={() => setIsOpen(!isOpen)}
+                className="user-icon flex cursor-pointer relative"
+                onClick={() =>
+                  status === "authenticated" ? setIsOpen(!isOpen) : ""
+                }
               >
                 <FaUserCircle size={30} />
               </span>
