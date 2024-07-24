07afa8b1176a25be2e65a0b848d7d50143b7981d
kaif siddiqui <kaifs8998@gmail.com>
2024-07-24 23:56:57 +0530

    feat-created utils folder and stored 'db.js' file for mongodb connection

---
+++ utils/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  if (mongoose.connections[0].readyState) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Successfully connected to MongoDB");
+  } catch (error) {
+    throw new Error("Error while connecting to MongoDB:", error);
+  }
+};
+
+export default connectDB;
