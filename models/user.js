27d6537cbe48bed0bc96df3b8c9f8f1f7f43062f
kaif siddiqui <kaifs8998@gmail.com>
2024-07-25 00:20:22 +0530

    feat-created models folder and stored 'user.js' file for create User-schema using mongoose

---
+++ models/user.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: false,
+    },
+    provider: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || mongoose.model("User", userSchema);
+
+export default User;
