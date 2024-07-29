4b48c8333f07f6364d534b56fdb8ea83a4f4f6ad
kaif siddiqui <kaifs8998@gmail.com>
2024-07-29 23:58:19 +0530

    feat-created a custom loading page component

---
+++ app/components/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex h-screen justify-center items-center text-gray-600 text-2xl max-lg:text-xl font-semibold tracking-wider">
+      Loading...
+    </div>
+  );
+};
+
+export default Loading;
