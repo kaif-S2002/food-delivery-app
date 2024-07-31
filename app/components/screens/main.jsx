988608bbc3d184442f229b671df8492fea0ccb07
kaif siddiqui <kaifs8998@gmail.com>
2024-07-31 23:33:32 +0530

    feat-added connecting to the mongoDB on first rendering of component using useEffect hook

--- app/components/screens/main.jsx
+++ app/components/screens/main.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Carousel from "../carousel";
 import Categories from "../categories";
 import MenuList from "../menuList";
+import connectDB from "@/utils/db";
+import { useSession } from "next-auth/react";
+import Loading from "../loading";
 
 const page = () => {
+  const { status } = useSession();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await connectDB();
+      } catch (error) {
+        console.error("Failed to connect to the database:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <Carousel />
-      <Categories />
-      <MenuList />
+      {status === "loading" ? (
+        <Loading />
+      ) : (
+        <>
+          <Carousel />
+          <Categories />
+          <MenuList />
+        </>
+      )}
     </>
   );
 };
