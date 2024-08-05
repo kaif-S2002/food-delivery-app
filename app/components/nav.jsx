555ca71039fe5b75a4345d3e576b4c19e1072616
kaif siddiqui <kaifs8998@gmail.com>
2024-08-05 21:19:58 +0530

    feat-created 'isAdminEmail' function for checking admin login details & passing isAdmin state as a props to the dropDownMenu component

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -4,80 +4,74 @@ import { PiBowlFoodFill } from "react-icons/pi";
 import { IoIosSearch } from "react-icons/io";
 import { CiShoppingCart } from "react-icons/ci";
 import { FaUserCircle } from "react-icons/fa";
-import { RiAdminFill } from "react-icons/ri";
 import { useSession } from "next-auth/react";
 import DropDownMenu from "./dropDownMenu";
 
 const nav = (props) => {
   const [isAdmin, setIsAdmin] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [fetching, setFetching] = useState(false);
   const session = useSession();
   const status = session.status;
 
   useEffect(() => {
-    if (status === "authenticated") {
-      const { email } = session.data.user;
-      if (email === "kaifs8998@gmail.com") {
-        setIsAdmin(true);
-      } else {
-        setIsAdmin(false);
-      }
-    } else {
-    }
-  }, [status]);
+    const isAdminEmail = async () => {
+      setFetching(true);
+      const adminEmail =
+        (await status) === "authenticated" ? session.data.user.email : null;
+      (await adminEmail) === "kaifs8998@gmail.com" || "junedkhan933@gmail.com"
+        ? setIsAdmin(true)
+        : setIsAdmin(false);
 
+      setFetching(false);
+    };
+    isAdminEmail();
+  }, [status]);
   return (
     <>
       <header>
-        <div className="nav-wrapper w-full h-fit fixed z-10 shadow top-0 bg-gray-100">
+        <div className="nav-wrapper w-full h-fit fixed z-20 border-b-2 top-0 bg-white">
           <div className="nav flex justify-between items-center py-4 px-10 font-semibold overflow-hidden select-none max-sm:px-5">
-            <div className="nav-icon flex justify-center items-center text-sm text-green-700 font-serif font-bold cursor-pointer">
+            <div className="nav-icon flex justify-center items-center text-sm  text-green-700 font-serif font-bold cursor-pointer">
               <a href="/" className=" flex justify-center items-center">
                 <span>FOOD</span>
                 <PiBowlFoodFill size={25} />
                 <span>BOWL</span>
               </a>
             </div>
-            <div className="nav-items flex gap-12 justify-between items-center txet-sm tracking-wider max-sm:gap-8">
+            <div className="nav-items flex gap-14 justify-between items-center txet-sm max-sm:gap-8">
               <span className=" flex gap-1 cursor-pointer max-md:hidden">
                 Search
                 <IoIosSearch size={24} />
               </span>
-              <span className="cart cursor-pointer max-md:hidden">
+              <span className="cart flex justify-center items-center cursor-pointer max-md:hidden relative">
                 <a href="/"></a>
-                <CiShoppingCart size={30} />
-              </span>
-              {isAdmin ? (
-                <span className="admin-icon flex gap-1 cursor-pointer">
-                  Admin
-                  <RiAdminFill size={18} className=" mt-0.5" />
+                Cart
+                <CiShoppingCart size={32} className=" pb-0.5" />
+                <span className=" text-green-600 text-[8px] pb-0.5 font-bold absolute right-[10px]">
+                  0
                 </span>
-              ) : (
-                ""
-              )}
-              {status !== "authenticated" ? (
-                <>
-                  <span className=" max-md:hidden">
-                    <a href="/login">Login</a>
-                  </span>
-                  <span className=" max-md:hidden">
-                    <a href="/register">Signup</a>
-                  </span>
-                </>
-              ) : (
-                ""
-              )}
+              </span>
 
               <span
-                className="user-icon flex cursor-pointer relative"
+                className="user-icon flex items-center gap-2 cursor-pointer relative"
                 onClick={() =>
                   status === "authenticated" ? setIsOpen(!isOpen) : ""
                 }
               >
+                {status === "authenticated" ? (
+                  <p href="" className=" text-sm">
+                    Hi, {session.data.user.name}
+                  </p>
+                ) : (
+                  <a href={status === "loading" ? "" : "/login"}>Sign In</a>
+                )}
                 <FaUserCircle size={30} />
               </span>
               <DropDownMenu
-                visible={isOpen ? "top-20 right-0" : "top-20 right-0 hidden"}
+                visible={isOpen ? " right-0" : " -right-full"}
+                close={() => setIsOpen(false)}
+                admin={isAdmin}
               />
             </div>
           </div>
